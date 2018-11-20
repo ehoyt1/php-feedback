0cae8e17afc5e3e8a0185f8ca6eda356e50045df
Kenneth Ethan Hoyt <khoyt2@butlercc.edu>
2018-11-20 17:20:44 -0600

    Added HTML form for starters

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>Feedback Form</title>
+  </head>
+  <body>
+    <h1>Feedback Form</h1>
+    <p>Please fill out your feedback</p>
+    <form>
+      <label for="firstname">First Name</label>
+      <input type="text" name="firstname">
+      <label for="lastname">Last Name</label>
+      <input type="text" name="lastname"><br>
+      <label for="email">Email Address</label>
+      <input type="text" name="email">
+      <label for="phone">Phone Number</label>
+      <input type="text" name="phone"><br>
+      <label for="feedback">Please enter your feedback</label><br>
+      <textarea name="feedback"></textarea><br><br>
+      <input type="submit" name="submit">
+      <input type="reset" name="clear">
+    </form>
+  </body>
+</html>
