e745751fad9522a8cfb69878487899707194cce4
Kenneth Ethan Hoyt <khoyt2@butlercc.edu>
2018-12-04 19:07:45 -0600

    Implementing WCAG 2.0 Techniques

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html>
+<html lang="en">
   <head>
     <title>Feedback Form</title>
     <style type="text/css">
@@ -23,6 +23,11 @@
           -ms-grid-template-columns: none;
         }
       }
+      /* 2.4.7 - Focus Visible - Level AA WCAG 2.0 */
+      input:focus, textarea:focus {
+        color:#00f;
+        background-color:#ddd;
+      }
     </style>
   </head>
   <body>
@@ -31,19 +36,19 @@
     <form>
       <div class='container'>
         <label for="firstname">First Name</label>
-        <input type="text" name="firstname">
+        <input type="text" name="firstname" id="firstname" aria-label="First Name">
         <label for="lastname">Last Name</label>
-        <input type="text" name="lastname">
+        <input type="text" name="lastname" id="lastname" aria-label="Last Name">
         <label for="email">Email Address</label>
-        <input type="text" name="email">
+        <input type="text" name="email" id="email" aria-label="Email Address">
         <label for="phone">Phone Number</label>
-        <input type="text" name="phone">
+        <input type="text" name="phone" id="phone" aria-label="Phone Number">
         <label for="feedback">Please enter your feedback</label>
-        <textarea name="feedback" rows="5"></textarea>
+        <textarea name="feedback" id="feedback" aria-label="Enter your feedback" rows="5"></textarea>
       </div>
       <br>
-      <input type="submit" name="submit">
-      <input type="reset" name="clear">
+      <input type="submit" name="submit" aria-label="Submit form">
+      <input type="reset" name="clear" aria-label="Clear form">
     </form>
   </body>
 </html>
